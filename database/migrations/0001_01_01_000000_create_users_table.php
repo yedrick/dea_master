7a79dfd3063da2651aedf99adc61c9fee373feca
Zaidafons <zaida.gc.26@gmail.com>
2024-11-25 22:27:38 -0400

    Implementacion del formulario

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('ci');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
